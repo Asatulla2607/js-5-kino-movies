6556baec6df4a3d309b520c8cc01a71679ca4e7e
Asatulla2607 <asatulla.muktarov260700@gmail.com>
2025-02-19 16:33:13 +0600

    create images home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,9 @@
-import LOGO from '../img/homelogo.jpg'
-const Home = () =>{
-    
-    return(
-      <div>
-        <img width={1300} src={LOGO} alt='img'/>
-      </div>  
-    )
-}
-export default Home
+import LOGO from "../img/homelogo.png";
+const Home = () => {
+  return (
+    <div>
+      <img width={1300} src={LOGO} alt="img" />
+    </div>
+  );
+};
+export default Home;
